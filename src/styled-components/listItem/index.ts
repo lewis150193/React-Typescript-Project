b6ebad39f061042c5609f9fa64c3d66b1e54311d
Lewis Williams <lewiswilliams1501@gmail.com>
2019-04-16 22:07:04 +0100

    Added styled components

--- src/styled-components/listItem/index.ts
+++ src/styled-components/listItem/index.ts
@@ -1,11 +1,10 @@
 import styles from 'styled-components';
 
 export const ListItem = styles.li`
-     background: transparent;
+  background: transparent;
   border-radius: 3px;
   border: 2px solid palevioletred;
   color: palevioletred;
   margin: 0.5em 1em;
   padding: 0.25em 1em;
-
     `;
